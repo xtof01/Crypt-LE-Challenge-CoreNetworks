d238ffcc9bfcd5f9611c62b58922e7ed28b83206
Christof Efkemann <christof@efkemann.net>
2018-04-23 23:42:40 +0200

    initial

---
+++ lib/Crypt/LE/Challenge/CoreNetworks.pm
@@ -0,0 +1,99 @@
+package Crypt::LE::Challenge::CoreNetworks;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+use Digest::SHA 'sha256';
+use MIME::Base64 'encode_base64url';
+use Net::DNS::CoreNetworks;
+
+
+sub new {
+    my $class = shift;
+    my $self = bless {}, $class;
+    $self->{client} = Net::DNS::CoreNetworks->new();
+    return $self;
+}
+
+sub handle_challenge_http {
+    return 0;
+}
+
+sub handle_challenge_tls {
+    return 0;
+}
+
+sub handle_challenge_dns {
+    my $self = shift;
+    my ($challenge, $params) = @_;
+    $challenge->{logger}->info("Processing the 'dns' challenge for '$challenge->{domain}' with " . __PACKAGE__) if $challenge->{logger};
+    my $value = encode_base64url(sha256("$challenge->{token}.$challenge->{fingerprint}"));
+    my (undef, $domain) = $challenge->{domain} =~ /^(\*\.)?(.+)$/;
+    my $fqdn = '_acme-challenge.' . $domain;
+
+    if ($self->{client}->login($ENV{CORENETWORKS_USER}, $ENV{CORENETWORKS_PASSWORD})) {
+        # find zone
+        my @zones = map $_->{name}, grep $_->{type} eq "master", $self->{client}->zones();
+
+        foreach my $zonename (_lsort(@zones)) {
+            if ($fqdn =~ /^(.*)\.\Q$zonename\E$/) {
+                my $rr = $1;
+                my $zone = $self->{client}->zone($zonename);
+
+                # create record
+                $challenge->{logger}->info("Creating $rr TXT record in zone $zonename") if $challenge->{logger};
+                if ($zone->add($rr, 60, 'TXT', $value)) {
+                    $self->{cleanup}->{$challenge->{domain}}->{zone} = $zonename;
+                    $self->{cleanup}->{$challenge->{domain}}->{rr} = $rr;
+                    if ($zone->commit()) {
+                        sleep(1);
+                        return 1;
+                    }
+                }
+
+                last;
+            }
+        }
+
+    }
+    else {
+        return 0;
+    }
+}
+
+sub handle_verification_http {
+    return 0;
+}
+
+sub handle_verification_tls {
+    return 0;
+}
+
+sub handle_verification_dns {
+    my $self = shift;
+    my ($results, $params) = @_;
+    $results->{logger}->info("Processing the 'http' verification for '$results->{domain}' with " . __PACKAGE__) if $results->{logger};
+    if ($results->{valid}) {
+        print "Domain verification results for '$results->{domain}': success.\n";
+    } else {
+        print "Domain verification results for '$results->{domain}': error. $results->{error}\n";
+    }
+    my $zonename = $self->{cleanup}->{$results->{domain}}->{zone};
+    my $rr = $self->{cleanup}->{$results->{domain}}->{rr};
+    my $zone = $self->{client}->zone($zonename);
+    $results->{logger}->info("Deleting $rr TXT record in zone $zonename") if $challenge->{logger};
+    if ($zone->remove($rr, undef, 'TXT', undef)) {
+        if ($zone->commit()) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+sub _lsort {
+    return sort { length($b) <=> length($a) } @_;
+}
+
+1;
